1c4fc6a9e9f931f6f1f15394c9cbf71ea28a2a51
sl <sl@anhot.ru>
2020-05-02 02:17:46 +0500

    JS implementation of bin2tap used

--- tools/maketape.sh
+++ tools/maketape.sh
@@ -8,7 +8,7 @@ zmakebas -i 1 -a 10 -l -o ${TAPE} -n ${PROJECT} program.bas
 
 for SCR in assets/*.scr; do
   NAME=$(basename $SCR ".scr")
-  bin2tap -o $NAME -a 16384 $SCR
+  jsbin2tap -o $NAME -a 16384 $SCR
   cat $NAME >> ${TAPE}
   rm ${NAME}
 done
